187083e26cd4cca4e11d67a4c95958e79e3ef3f4
Israel Toledo <israel@omr.mx>
2019-07-11 17:23:20 -0500

    se cambio module colors de imagen a check

--- src/components/DisenoGrafico.vue
+++ src/components/DisenoGrafico.vue
@@ -2,16 +2,48 @@
   <div>
     <h1>Diseño Gráfico</h1>
     <div class="col-md-8 offset-md-2">
-        <upload-file v-bind:id="'bio'" v-bind:path="'diseno_grafico'" v-bind:label="'Foto Diseño'" v-bind:width="150"></upload-file>
-        <br />
-        <upload-file v-bind:id="'prod'" v-bind:path="'diseno_grafico'" v-bind:label="'Foto Producto'" v-bind:width="150"></upload-file>
-        <br />
-        <upload-file v-bind:id="'mod'" v-bind:path="'diseno_grafico'" v-bind:label="'Module Colors'" v-bind:width="150"></upload-file>
-        <br />
-        <upload-file v-bind:id="'dim'" v-bind:path="'diseno_grafico'" v-bind:label="'Dimensions'" v-bind:width="150"></upload-file>
-        <br />
-        <upload-file v-bind:id="'est'" v-bind:path="'diseno_grafico'" v-bind:label="'Estructura'" v-bind:width="150"></upload-file>
-      
+      <upload-file
+        v-bind:id="'bio'"
+        v-bind:path="'diseno_grafico'"
+        v-bind:label="'Foto Diseño'"
+        v-bind:width="150"
+      ></upload-file>
+      <br />
+      <upload-file
+        v-bind:id="'prod'"
+        v-bind:path="'diseno_grafico'"
+        v-bind:label="'Foto Producto'"
+        v-bind:width="150"
+      ></upload-file>
+      <br />
+      <upload-file
+        v-bind:id="'dim'"
+        v-bind:path="'diseno_grafico'"
+        v-bind:label="'Dimensions'"
+        v-bind:width="150"
+      ></upload-file>
+      <br />
+      <upload-file
+        v-bind:id="'est'"
+        v-bind:path="'diseno_grafico'"
+        v-bind:label="'Estructura'"
+        v-bind:width="150"
+      ></upload-file>
+      <br />
+      <h4>Module colors</h4>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="black" id="Black" v-model="moduleColors" />
+        <label class="form-check-label" for="Black">Black</label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="silver" id="Silver"  v-model="moduleColors"/>
+        <label class="form-check-label" for="Silver">Silver</label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="gold" id="Gold"  v-model="moduleColors"/>
+        <label class="form-check-label" for="Gold">Gold</label>
+      </div>
+      <button class="btn btn-primary pull-right text-right" @click="guardar"> Guardar </button>
     </div>
     <br />
   </div>
@@ -21,16 +53,18 @@
 <script>
 import { db } from "@/main";
 import router from "@/router";
-import firebase from 'firebase';
-import UploadFileVue from './UploadFile.vue';
+import firebase from "firebase";
+import UploadFileVue from "./UploadFile.vue";
 export default {
   name: "diseno-grafico",
   props: {},
   components: {
-      "upload-file": UploadFileVue
+    "upload-file": UploadFileVue
   },
   data() {
-    return {};
+    return {
+        moduleColors: []
+    };
   },
   created() {
     console.log(this.$route.params.projectid);
@@ -41,7 +75,14 @@ export default {
     }
   },
   methods: {
-    
+      guardar() {
+          db.collection("projects").doc(this.projectid).update ({
+              moduleColors: this.moduleColors
+          }).then (()=>{
+              alert ("Proyecto Actualizado");
+              this.$router.push("/home");
+          });
+      }
   }
 };
 </script>
