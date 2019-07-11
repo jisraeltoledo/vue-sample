bd15b277c60345d6eecad0bbec95d1c243c2fdcb
Israel Toledo <israel@omr.mx>
2019-07-11 10:17:05 -0500

    Listado de proyectos listo

--- src/components/moderador/ListProject.vue
+++ src/components/moderador/ListProject.vue
@@ -1,14 +1,30 @@
 <template>
   <div>
-    <h1>Proyectos</h1>
-    <div class="col-md-6 offset-md-3">
-      <table>
-          <tr v-for="(project, idx) in projects" :key="idx">
-              <td>{{project.nombre}}</td>
-              <td>{{project.modelo}}</td>
-              <td>{{project.descripcion}}</td>
-              <td>{{project.bio}}</td>
+    <h1>Proyectos 2</h1>
+    <div class="col-md-8 offset-md-2">
+      <table class="table thead-dark">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Modelo</th>
+            <th scope="col">Descripción</th>
+            <th scope="col">Biografía</th>
           </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(project, idx) in projects"
+            :key="idx"
+            class="clickable-row"
+            v-on:click="clickList(project)"
+            style="cursor: pointer; "
+          >
+            <td>{{project.nombre}}</td>
+            <td>{{project.modelo}}</td>
+            <td>{{project.descripcion}}</td>
+            <td>{{project.bio}}</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   </div>
@@ -26,16 +42,19 @@ export default {
       projects: []
     };
   },
-  create (){
-      db.collection ("projects").onSnapshot (snap=>{
-          snap.forEach(element => {
-            this.projects.push (element.data ()); 
-          });          
+  created() {
+    console.log("proyectos", this.projects);
+    db.collection("projects").onSnapshot(snap => {
+      snap.forEach(element => {
+        this.projects.push(element.data());
       });
-      console.log (projects);
+    });
+    console.log(this.projects);
   },
   methods: {
-    
+    clickList(project) {
+      alert(project);
+    }
   }
 };
 </script>
