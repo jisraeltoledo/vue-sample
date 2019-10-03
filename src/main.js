42ee5ed5df5a97bc3082092334b0e33df1528aad
J. Israel  Toledo <j.israel.toledo@gmail.com>
2019-10-03 15:44:27 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -47,13 +47,7 @@ Vue.mixin({
 
 let app = "";
 const firebaseConfig = {
-  apiKey: "AIzaSyCb0lXfuHx5yQTzKb40gsLKvVo0dVqAfpk",
-  authDomain: "decimal-fic-tec.firebaseapp.com",
-  databaseURL: "https://decimal-fic-tec.firebaseio.com",
-  projectId: "decimal-fic-tec",
-  storageBucket: "decimal-fic-tec.appspot.com",
-  messagingSenderId: "465869946229",
-  appId: "1:465869946229:web:c7281894f9eafe0a"
+  
 };
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
